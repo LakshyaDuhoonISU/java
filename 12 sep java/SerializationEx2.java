8f0669117b7f69ed79a43984e623555f7b80487c
Lakshya Duhoon <2023.lakshyad@isu.ac.in>
2024-09-12 11:03:44 +0530

    edit

---
+++ 12 sep java/SerializationEx2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+class Dog implements Serializable {
+    String breed;
+    String name;
+
+    Dog(String breed, String name) {
+        this.breed = breed;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "breed = " + this.breed + ", name = " + this.name;
+    }
+}
+public class SerializationEx2 {
+    public static void main(String[] args) throws IOException {
+        Dog s1 = new Dog("German", "Tommy");
+        try (ObjectOutputStream os = new ObjectOutputStream(
+                new FileOutputStream("/Users/lakshyaduhoon/Documents/java/12 sep java/Dog.ser"));) {
+            os.writeObject(s1);
+        }
+        try (ObjectInputStream os = new ObjectInputStream(
+                new FileInputStream("/Users/lakshyaduhoon/Documents/java/12 sep java/Dog.ser"));) {
+            Dog s = (Dog) os.readObject();
+            System.out.println(s);
+        } catch (IOException e) {
+            System.out.println("Exception in Serialization: " + e);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Exception in Deserialization: " + e);
+        }
+    }
+}

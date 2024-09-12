f9fa4d7049cfdb68c3b664d3ef3ed466a5dbd0ee
Lakshya Duhoon <2023.lakshyad@isu.ac.in>
2024-09-12 11:27:33 +0530

    edit

--- 12 sep java/SerializationEx1Static.java
+++ 12 sep java/SerializationEx1Static.java
@@ -3,11 +3,13 @@ import java.io.*;
 class Student2 implements Serializable {
     int rollno;
     String name;
-    static String college="SSS";
-    public Student2(int rollno,String name) {
+    static String college = "SSS";
+
+    public Student2(int rollno, String name) {
         this.rollno = rollno;
         this.name = name;
     }
+
     public String toString() {
         return "rollno = " + this.rollno + ", name = " + this.name + ", college = " + college;
     }
@@ -15,20 +17,22 @@ class Student2 implements Serializable {
 
 public class SerializationEx1Static {
     public static void main(String[] args) {
-        Student2 s=new Student2(111, "AAA");
-        Student2.college="ttt";
-        try(ObjectOutputStream os=new ObjectOutputStream(new FileOutputStream("/Users/lakshyaduhoon/Documents/java/12 sep java/SerialStud.ser"));) {
+        Student2 s = new Student2(111, "AAA");
+        Student2.college = "ttt";
+        try (ObjectOutputStream os = new ObjectOutputStream(
+                new FileOutputStream("/Users/lakshyaduhoon/Documents/java/12 sep java/SerialStud.ser"));) {
             os.writeObject(s);
         } catch (IOException e) {
-            System.out.println("Exception in Serialization: "+e);
+            System.out.println("Exception in Serialization: " + e);
         }
-        try(ObjectInputStream os=new ObjectInputStream(new FileInputStream("/Users/lakshyaduhoon/Documents/java/12 sep java/SerialStud.ser"));) {
-            Student2 s1=(Student2)os.readObject();
+        try (ObjectInputStream os = new ObjectInputStream(
+                new FileInputStream("/Users/lakshyaduhoon/Documents/java/12 sep java/SerialStud.ser"));) {
+            Student2 s1 = (Student2) os.readObject();
             System.out.println(s1);
         } catch (IOException e) {
-            System.out.println("Exception in Deserialization: "+e);
+            System.out.println("Exception in Deserialization: " + e);
         } catch (ClassNotFoundException e) {
-            System.out.println("Exception in Deserialization: "+e);
+            System.out.println("Exception in Deserialization: " + e);
         }
     }
 }

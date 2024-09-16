a79113a40f71df26e402c453b7a62960b18835f9
Lakshya Duhoon <2023.lakshyad@isu.ac.in>
2024-09-16 23:05:18 +0530

    edit

--- 12 sep java/ProducerConsumerImplementation2.java
+++ 12 sep java/ProducerConsumerImplementation2.java
@@ -2,43 +2,47 @@
 
 class Q {
     int n;
-    boolean isValueSet=false;
+    boolean isValueSet = false;
+
     synchronized int consume() {
-        while(!isValueSet){
-            try{
+        while (!isValueSet) {
+            try {
                 wait();
-            }
-            catch(InterruptedException e) {
+            } catch (InterruptedException e) {
                 System.out.println("Wait interrupted");
             }
         }
-        System.out.println("Get: "+n);
+        System.out.println("Get: " + n);
         System.out.println();
-        isValueSet=false;
+        isValueSet = false;
         notify();
         return n;
     }
+
     synchronized void produce(int n) {
         while (isValueSet) {
-            try{wait();}
-            catch(InterruptedException e) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
                 System.out.println("Wait interrupted");
             }
         }
-        this.n=n;
-        isValueSet=true;
-        System.out.println("Put: "+n);
+        this.n = n;
+        isValueSet = true;
+        System.out.println("Put: " + n);
         notify();
     }
 }
 
-class Producer implements Runnable{
+class Producer implements Runnable {
     Q q;
+
     Producer(Q q) {
-        this.q=q;
+        this.q = q;
     }
+
     public void run() {
-        int i=0;
+        int i = 0;
         while (true) {
             q.produce(i++);
             try {
@@ -50,11 +54,13 @@ class Producer implements Runnable{
     }
 }
 
-class Consumer implements Runnable{
+class Consumer implements Runnable {
     Q q;
+
     Consumer(Q q) {
-        this.q=q;
+        this.q = q;
     }
+
     public void run() {
         while (true) {
             q.consume();
@@ -69,11 +75,11 @@ class Consumer implements Runnable{
 
 public class ProducerConsumerImplementation2 {
     public static void main(String[] args) {
-        Q q=new Q();
-        Producer p=new Producer(q);
-        Consumer c=new Consumer(q);
-        Thread t1=new Thread(p, "Producer");
-        Thread t2=new Thread(c, "Consumer");
+        Q q = new Q();
+        Producer p = new Producer(q);
+        Consumer c = new Consumer(q);
+        Thread t1 = new Thread(p, "Producer");
+        Thread t2 = new Thread(c, "Consumer");
         t1.start();
         t2.start();
     }

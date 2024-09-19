672245321f251535325a701e8f9068a647a389fe
Lakshya Duhoon <2023.lakshyad@isu.ac.in>
2024-09-19 11:41:53 +0530

    edit

--- 19 sep java/Ex1SquareCalculator.java
+++ 19 sep java/Ex1SquareCalculator.java
@@ -11,8 +11,10 @@ public class Ex1SquareCalculator {
         JTextField tf = new JTextField(10);
         JLabel l = new JLabel("Enter a number: ");
 
-        tf.setBounds(50, 50, 150, 20);
-        b.setBounds(100, 50, 95, 30);
+        l.setBounds(50, 50, 150, 20);
+        tf.setBounds(150, 50, 150, 20);
+        b.setBounds(50, 100, 150, 30);
+        b1.setBounds(200, 100,95,30);
 
         b.addActionListener(
                 new ActionListener() {
@@ -34,7 +36,7 @@ public class Ex1SquareCalculator {
         f.add(b);
         f.add(b1);
         f.setSize(400, 500);
-        f.setLayout(new FlowLayout());
+        f.setLayout(null);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }

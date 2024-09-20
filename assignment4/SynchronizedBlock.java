47606ce905a372b677cfa71176b2cb5ec556b9c6
Lakshya Duhoon <2023.lakshyad@isu.ac.in>
2024-09-20 16:02:07 +0530

    edit

---
+++ assignment4/SynchronizedBlock.java
@@ -0,0 +1,46 @@
+// Q5 - Write a Java program to demonstrate the concept of synchronized blocks.
+
+class Counter {
+    private int count = 0;
+
+    public void increment() {
+        synchronized (this) {
+            count++;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+class MyThread extends Thread {
+    Counter counter;
+
+    public MyThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
+
+public class SynchronizedBlock {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        MyThread t1 = new MyThread(counter);
+        MyThread t2 = new MyThread(counter);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Final count: " + counter.getCount());
+    }
+}
